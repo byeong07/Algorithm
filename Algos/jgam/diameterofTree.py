fe8ffceeae1550817f2e09633664c05d6f591ced
jgam <jgam@alumni.nd.edu>
2019-12-13 19:25:38 +0900

    Update diameterofTree.py

--- Algos/jgam/diameterofTree.py
+++ Algos/jgam/diameterofTree.py
@@ -22,31 +22,3 @@ def bfs(graph_list, start):
     
 #list creation for sorting
 bfs_node = bfs(tree_info, 1)
-max_node = [0 for _ in range(node + 1)]
-diameter = [[0] for _ in range(node + 1)]
-max_diameter = [0 for _ in range(node + 1)]
-
-
-
-#from the bottom of node, diameters extracted
-while bfs_node:
-    temp = bfs_node.pop()
-    
-    for child in tree_info[temp]:
-        try:
-            child_length = max_node[child[0]] + child[1]
-        except:
-            child_length = child[1]
-        diameter[temp].append(child_length)
-        max_node[temp] = max(diameter[temp])
-        
-    max_diameter[temp] += max(diameter[temp])
-    diameter[temp].remove(max(diameter[temp]))
-    try:
-        max_diameter[temp] += max(diameter[temp])
-    except:
-        pass
-        
-
-#maximum diameter from diameters
-print(max(max_diameter))
